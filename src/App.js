de8b15d34cd9141b4139295e46aa5684380a71c8
Chandra <532chandu@gmail.com>
2020-11-06 18:17:30 -0500

    Adding a ListEmployeeComponent for displaying Employee List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world!</h1>
-      </header>
+    <div className="container">
+      <ListEmployeeComponent/>
     </div>
   );
 }
